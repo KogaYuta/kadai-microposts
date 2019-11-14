d9242547195c3aad691daef6ac6adeb12e6c63bf
yuta.koga <with.lightning.speed@docomo.ne.jp>
2019-11-14 12:00:00 +0000

    follow, unfollow

--- app/User.php
+++ app/User.php
@@ -31,4 +31,47 @@ class User extends Authenticatable
         // ユーザーはMicropostを複数持つ
         return $this->hasMany(Micropost::class);
     }
+    
+    public function followings() {
+        // クラス名、中間テーブル、リレーションを定義しているモデル(自身)の外部キー名, 結合するモデルの外部キー(相手)のid
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+    
+    public function followers() {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+    
+    public function follow($userId) {
+        $exist = $this->is_following($userId);
+        
+        $its_me = $this->id == $userId;
+        
+        if ($exist || $its_me) {
+            return false;
+        } else {
+            $this->followings()->attach($userId);
+            return true;
+        }
+        
+    }
+    
+    public function unfollow($userId) {
+        $exist = $this->is_following($userId);
+        
+        $its_me = $this->id == $userId;
+        
+        if ($exist && !$its_me) {
+            // 既にフォローしていればフォローを外す
+            $this->followings()->detach($userId);
+            return true;
+        } else {
+            // 未フォローであれば何もしない
+            return false;
+        }
+    }
+    
+    public function is_following($userId) {
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
+        
 }
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,14 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 Route::group(['middleware' => ['auth']], function() {
     // 以下に書かれたroutingはログイン認証が必須となる, 'only'で実装するアクションを選択している
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    
+    Route::group(['prefix' => 'users/{id}'], function () {
+        // prefixによりURL後にusers/{id}がつき、その後ろにfollow等がつく
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+    
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
